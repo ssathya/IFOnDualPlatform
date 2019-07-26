4b4fbabe6a84e0fb5e5d3f2d4c2ffadd331679d2
Sridhar Sathya <sridhar.sathya@live.com>
2019-07-26 16:15:39 -0400

    End of day July 7th.

---
+++ Utilities/Amazon/S3/ReadS3Objects.cs
@@ -0,0 +1,126 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Utilities.StringHelpers;
+
+namespace Utilities.Amazon.S3
+{
+
+	public class ReadS3Objects
+	{
+
+		#region Private Fields
+
+		private readonly string _bucketName;
+		private readonly RegionEndpoint _region;
+		private const int BufferSize = 32768;
+		#endregion Private Fields
+
+
+		#region Public Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReadS3Objects"/> class.
+		/// </summary>
+		/// <param name="bucketName">Name of the bucket.</param>
+		/// <param name="region">The region.</param>
+		public ReadS3Objects(string bucketName, RegionEndpoint region)
+		{
+			_bucketName = bucketName;
+			_region = region;
+			if (_region == null)
+			{
+				_region = RegionEndpoint.USEast2;
+			}
+		}
+
+		#endregion Public Constructors
+
+
+		#region Public Methods		
+		/// <summary>
+		/// Gets the data from s3.
+		/// Useful to read ASCII files.
+		/// </summary>
+		/// <param name="objectName">Name of the object.</param>
+		/// <returns></returns>
+		public async Task<string> GetDataFromS3(string objectName)
+		{
+			var responseBody = "";
+			try
+			{
+				var client = new AmazonS3Client(_region);
+				var request = new GetObjectRequest
+				{
+					BucketName = _bucketName,
+					Key = objectName
+				};
+				using (var response = await client.GetObjectAsync(request))
+				{
+					using (var responseStream = response.ResponseStream)
+					{
+						using (var reader = new StreamReader(responseStream))
+						{
+							responseBody = await reader.ReadToEndAsync();
+						}
+					}
+				}
+				return responseBody;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Something went wrong while reading S3");
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("Terminating application");
+				throw;
+			}
+		}
+		public async Task<string> GetEncryptedDataFromS3(string objectName)
+		{
+			var returnString = await GetDataFromS3(objectName);
+			returnString = returnString.Decrypt();
+			return returnString;
+		}
+
+		public async Task<FileStream> ReadExcelFile(string objectName)
+		{
+			var client = new AmazonS3Client(_region);
+			var request = new GetObjectRequest
+			{
+				BucketName = _bucketName,
+				Key = objectName
+			};
+			var fileName = GetTempFileName();
+			using (var fs = new FileStream(fileName, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None, BufferSize,
+				FileOptions.RandomAccess | FileOptions.DeleteOnClose))
+			{
+				using (GetObjectResponse response = await client.GetObjectAsync(request))
+				{
+					using (Stream responseStream = response.ResponseStream)
+					{
+						var data = new byte[BufferSize];
+						int bytesRead = 0;
+						do
+						{
+							bytesRead = responseStream.Read(data, 0, BufferSize);
+							fs.Write(data, 0, bytesRead);
+						}
+						while (bytesRead > 0);
+						return fs;
+					}
+				}				
+			}
+			
+		}
+
+		private string GetTempFileName()
+		{
+			return Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".xlsx");
+		}
+
+		#endregion Public Methods
+	}
+}
---
+++ Utilities/StringHelpers/StringExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Utilities.StringHelpers
+{
+	public static class StringExtensions
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Converts/removes all non-ASCII to ASCII.
+		/// </summary>
+		/// <param name="inString">The in string.</param>
+		/// <returns></returns>
+		public static string ConvertAllToASCII(string inString)
+		{
+			var newStringBuilder = new StringBuilder();
+			newStringBuilder.Append(inString.Normalize(NormalizationForm.FormKD)
+											.Where(x => x < 128)
+											.ToArray());
+			return newStringBuilder.ToString();
+		}
+
+		/// <summary>
+		/// Converts string to SSML.
+		/// </summary>
+		/// <param name="unformatedMsg">The unformated MSG.</param>
+		/// <returns></returns>
+		public static string ConvertToSSML(string unformatedMsg)
+		{
+			StringBuilder tempValue = new StringBuilder();
+			tempValue.Append("<speak>");
+			tempValue.Append(unformatedMsg);
+			tempValue.Append("</speak>");
+			var retValue = Regex.Replace(tempValue.ToString(), @"\r\n?|\n|\\n|\\r\\n", @"<break strength='x - strong' time='500ms' />");
+			return retValue;
+		}
+
+		/// <summary>
+		/// Determines whether string [is null or white space].
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>
+		///   <c>true</c> if [is null or white space] [the specified value]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool IsNullOrWhiteSpace(this string value)
+		{
+			return string.IsNullOrWhiteSpace(value);
+		}
+
+		/// <summary>
+		/// Replaces the first.
+		/// </summary>
+		/// <param name="text">The text.</param>
+		/// <param name="search">The search.</param>
+		/// <param name="replace">The replace.</param>
+		/// <returns></returns>
+		public static string ReplaceFirst(this string text, string search, string replace)
+		{
+			int pos = text.IndexOf(search);
+			if (pos < 0)
+			{
+				return text;
+			}
+			return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+		}
+
+		/// <summary>
+		/// Converts to Thousands, millions, and billions.
+		/// </summary>
+		/// <param name="num">The number.</param>
+		/// <returns></returns>
+		public static string ToKMB(this decimal num)
+		{
+			if (num > 999999999 || num < -999999999)
+			{
+				return num.ToString("0,,,.### Billions", CultureInfo.InvariantCulture);
+			}
+			else
+			if (num > 999999 || num < -999999)
+			{
+				return num.ToString("0,,.## Millions", CultureInfo.InvariantCulture);
+			}
+			else
+			if (num > 999 || num < -999)
+			{
+				return num.ToString("0,.# Thousands", CultureInfo.InvariantCulture);
+			}
+			else
+			{
+				return num.ToString(CultureInfo.InvariantCulture);
+			}
+		}
+
+		/// <summary>
+		/// Truncates a string at word rather than at a number.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="length">The length.</param>
+		/// <returns></returns>
+		public static string TruncateAtWord(this string value, int length)
+		{
+			if (value == null || value.Length < length || value.IndexOf(" ", length) == -1)
+				return value;
+
+			return value.Substring(0, value.IndexOf(" ", length));
+		}
+
+		#endregion Public Methods
+	}
+}
