894769f2b18e385773f146731af67b132efcc93f
Sridhar Sathya <sridhar.sathya@live.com>
2019-07-30 10:39:05 -0400

    Basic parsing of WebhookRequest works

---
+++ Models/Reqeusts/AppResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Models.Reqeusts
+{
+	public class AppResponse : IAppResponse
+	{
+		public AppResponse()
+		{
+			ResponseCreateTime = DateTime.Now;
+			IsResponseSsml = false;
+			IsResponseSuccess = false;
+		}
+		public string ResponseCreatedBy { get; set; }
+		public DateTime ResponseCreateTime { get; set; }
+		public bool IsResponseSsml { get; set; }
+		public string ResponseData { get; set; }
+		public bool IsResponseSuccess { get; set; }
+	}
+}
---
+++ Models/Reqeusts/CompanyNews.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Reqeusts
+{
+	public class CompanyNews : IAppRequest
+	{
+		public CompanyNews()
+		{
+			RequestStart = DateTime.Now;
+			IsSsmlResponseRequested = false;
+		}
+		public string RequestName { get; set; }
+		public DateTime RequestStart { get; set; }		
+		public string CompanyName { get; set; }
+		public bool IsSsmlResponseRequested { get; set; }
+	}
+}
